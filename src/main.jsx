206130a13d310a515e78df01b124e64ac74d93ec
Andrew <96084930+Andrewramo@users.noreply.github.com.>
2026-02-03 10:16:48 +0200

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,13 @@ import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import App from './App.jsx'
+import { BrowserRouter, HashRouter } from 'react-router-dom';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+
     <App />
+
   </StrictMode>,
 )
